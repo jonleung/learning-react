8d8f0c2fae75593e138db97fc0523f3e1606dfad
Jonathan <me@jonl.org>
2017-05-18 16:03:29 -0400

    split into admin and index via common code plugin

--- src/app.js
+++ src/app.js
@@ -13,11 +13,9 @@ img.style.padding = "20px"
 img.width = 32
 document.body.appendChild(img)
 
-
+import './style.scss'
 import {groupBy} from 'lodash/collection'
 import people from './people'
-
-import './style.scss'
 
 const managerGroups = groupBy(people, 'manager')
 
