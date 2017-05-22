dc07abc30a72ea280422dfdb4d9f395cd5b7e02b
Jonathan <me@jonl.org>
2017-05-21 20:33:34 -0400

    migrate main files for tic-tac-toe

--- src/app.js
+++ src/app.js
@@ -3,10 +3,4 @@ if (module.hot) {
 }
 
 import './style.scss'
-import {groupBy} from 'lodash/collection'
 
-import Todo from './todo'
-import TodoList from './todo_list'
-
-let x = new Todo("Hello World")
-console.log(x.title)
