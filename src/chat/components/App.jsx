934c38e328504689daa23ae08a26439859448994
Jonathan <me@jonl.org>
2017-05-26 12:44:00 -0400

    add prompt for name to chat app

--- src/chat/components/App.jsx
+++ src/chat/components/App.jsx
@@ -4,12 +4,27 @@ import PropTypes from 'prop-types';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {}
+    this.setName = this.setName.bind(this);
+  }
+
+  componentDidMount() {
+    if (this.state.name === undefined) {
+      const name = prompt('What is your name?');
+      this.setName(name);
+    }
+  }
+
+  setName(name) {
+    this.setState({
+      name,
+    });
   }
 
   render() {
     return (
       <div>
-        <h1>Hello World! 🐰</h1>
+        <h1>Hello {this.state.name}!</h1>
       </div>
     );
   }
